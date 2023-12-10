79864711e7a6b8fb9126908a9e341deb032e85c8
PorkChopExpress86 <29725019+PorkChopExpress86@users.noreply.github.com>
2023-12-10 12:30:39 -0600

    Added removing the folder and recreating it.

--- download_files.bash
+++ download_files.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+rm -rf zipped_data
+mkdir zipped_data
+
 # Download Real_building_land.zip file
 curl --tlsv1.1 https://download.hcad.org/data/CAMA/2023/Real_building_land.zip --output ./zipped_data/Real_building_land.zip
 
@@ -7,6 +10,9 @@ curl --tlsv1.1 https://download.hcad.org/data/CAMA/2023/Real_building_land.zip -
 curl --tlsv1.1 https://download.hcad.org/data/CAMA/2023/Real_acct_owner.zip --output ./zipped_data/Real_acct_owner.zip
 
 # Unzip files
+rm -rf ./text_files
+mkdir text_files
+
 unzip -o zipped_data/Real_acct_owner.zip "real_acct.txt" -d text_files
 
 unzip -o zipped_data/Real_building_land.zip "building_res.txt" -d text_files
@@ -14,5 +20,3 @@ unzip -o zipped_data/Real_building_land.zip "building_res.txt" -d text_files
 
 # Run command to create the database files and tables
 python3.12 insert_data.py
-
-sqlite3 -separator ',' stuff.db ".import myfile.csv t_table_name"
