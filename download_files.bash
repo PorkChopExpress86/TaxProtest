908d22dfa646bf4b9810bf5f832788a02177c7c5
PorkChopExpress86 <blake.b.8726@gmail.com>
2023-12-11 23:33:29 -0600

    run python without a version because it is in a venv

--- download_files.bash
+++ download_files.bash
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 rm -rf zipped_data
 mkdir zipped_data
 
 # Download Real_building_land.zip file
-curl --tlsv1.1 https://download.hcad.org/data/CAMA/2023/Real_building_land.zip --output ./zipped_data/Real_building_land.zip
+curl --tlsv1.1 --insecure https://download.hcad.org/data/CAMA/2023/Real_building_land.zip --output ./zipped_data/Real_building_land.zip
 
 # Download Real_acct_owner.zip file
-curl --tlsv1.1 https://download.hcad.org/data/CAMA/2023/Real_acct_owner.zip --output ./zipped_data/Real_acct_owner.zip
+curl --tlsv1.1 --insecure https://download.hcad.org/data/CAMA/2023/Real_acct_owner.zip --output ./zipped_data/Real_acct_owner.zip
 
 # Unzip files
 rm -rf ./text_files
@@ -19,6 +19,6 @@ unzip -o zipped_data/Real_building_land.zip "building_res.txt" -d text_files
 
 
 # Run command to create the database files and tables
-python3.12 insert_data.py
+python insert_data.py
 
 mkdir Exports
